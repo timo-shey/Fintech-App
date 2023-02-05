532c34bf2283fb43eedf1348ebdf1add26a89863
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2023-02-05 16:22:53 +0100

    chore: Credit Controller

--- src/main/java/com/example/FintechApplication/controller/CreditController.java
+++ src/main/java/com/example/FintechApplication/controller/CreditController.java
@@ -1,10 +1,45 @@
 package com.example.FintechApplication.controller;
 
+import com.example.FintechApplication.dto.request.CreditRequest;
+import com.example.FintechApplication.dto.request.CreditUpdateRequest;
+import com.example.FintechApplication.dto.response.CreditResponse;
+import com.example.FintechApplication.exceptions.CustomerNotFoundException;
+import com.example.FintechApplication.service.CreditService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class CreditController {
     private final CreditService creditService;
+
+    @PostMapping("/credits/{id}")
+    public CreditResponse createCreditForCustomer(
+            @PathVariable(name ="id") String customerId,
+            @RequestBody CreditRequest newCreditRequest
+    )throws CustomerNotFoundException {
+        return creditService.createCreditForCustomer(customerId, newCreditRequest);
+    }
+
+    @GetMapping("/credits/{id}")
+    public List<CreditResponse> getAllCredits(
+            @PathVariable (name="id") String customerId
+    ){
+        return creditService.getAllCreditsById(customerId);
+    }
+
+    @PutMapping("/credits/{id}")
+    public CreditResponse payoffCredit(
+            @PathVariable String id,
+            @RequestBody CreditUpdateRequest creditUpdateRequest
+    ) throws Exception{
+        return creditService.createUpdateCredit(id, creditUpdateRequest);
+    }
+
+    @GetMapping("/credits")
+    public List<CreditResponse> getAllCredits(){
+        return creditService.getAllCredits();
+    }
 }
